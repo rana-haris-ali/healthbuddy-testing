64e1167aef63385fa1dfc165ce3fe78d60435515
me <ranaharisali42@yahoo.com>
2021-05-03 08:23:41 +0500

    added HomeScreen and Product components, displayed dummy products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,20 @@
+import { Container } from 'react-bootstrap';
+import Header from './components/pharmacy/Header';
+import Footer from './components/pharmacy/Footer';
+import HomeScreen from './screens/pharmacy/HomeScreen';
+
 const App = () => {
-  return <div>HealthBuddy</div>;
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
