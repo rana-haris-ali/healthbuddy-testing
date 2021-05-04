8bb14ba687bd353c222874d3ffdd16289e97c9b8
me <ranaharisali42@yahoo.com>
2021-05-04 08:07:01 +0500

    add react-router, create initial routes, replace anchor tags with Link components

--- frontend/src/components/pharmacy/Product.js
+++ frontend/src/components/pharmacy/Product.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
 import Rating from './Rating';
 
 // render a single product as card
@@ -7,15 +9,15 @@ import Rating from './Rating';
 const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <Card.Img className='card-img-top' variant='top' src={product.image} />
-      </a>
+      </Link>
       <Card.Body>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as='div'>
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
         <Card.Text as='div'>
           <Rating
             value={product.rating}
