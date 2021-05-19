78b14b07c694f5d948224eaa735a2805c304a537
Rana Haris Ali <64707886+rana-haris-ali@users.noreply.github.com>
2021-05-19 23:19:45 +0500

    add authMiddleware, create user registeration and login routes and functionality

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -30,8 +30,21 @@ const userSchema = mongoose.Schema(
 	}
 );
 
+// method to compare entered password with saved password
 userSchema.methods.matchPassword = async function (enteredPassword) {
 	return await bcrypt.compare(enteredPassword, this.password);
 };
+
+userSchema.pre('save', async function (next) {
+	// this conditional checks if the user is modifing
+	// only username and/or email without modifing password
+	// and hence prevents re-hashing of the already hashed password
+	if (!this.isModified('password')) {
+		next();
+	}
+
+	const salt = await bcrypt.genSalt(10);
+	this.password = await bcrypt.hash(this.password, salt);
+});
 
 export default mongoose.model('User', userSchema);
