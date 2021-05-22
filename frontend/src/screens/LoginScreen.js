19774e582482d6ba7485e18df38f3b5b3c91d374
Rana Haris Ali <64707886+rana-haris-ali@users.noreply.github.com>
2021-05-22 09:08:52 +0500

    clean unused imports

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Row, Col, Form, Button, Container } from 'react-bootstrap';
+import { Row, Col, Form, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
