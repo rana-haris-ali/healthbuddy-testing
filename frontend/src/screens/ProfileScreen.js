35d412f111a99e64f841be9e18ac72b0a0860a97
Rana Haris Ali <64707886+rana-haris-ali@users.noreply.github.com>
2021-05-22 09:14:53 +0500

    setup profile screen and get userDetails from backend

---
+++ frontend/src/screens/ProfileScreen.js
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from 'react';
+import { Row, Col, Form, Button } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import { getUserDetails } from '../actions/pharmacy/userActions';
+// import { register } from '../actions/pharmacy/userActions';
+
+const ProfileScreen = ({ location, history }) => {
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [confirmPassword, setConfirmPassword] = useState('');
+	const [message, setMessage] = useState('');
+
+	const dispatch = useDispatch();
+	const { userInfo } = useSelector((state) => state.userLogin);
+
+	const userDetails = useSelector((state) => state.userDetails);
+	const { loading, error, user } = userDetails;
+
+	useEffect(() => {
+		if (!userInfo) {
+			// if user isn't logged in, redirect to login
+			history.push('/login');
+		} else {
+			// if user details havent been fetched yet, then fetch
+			if (!user.name) {
+				dispatch(getUserDetails('profile'));
+			} else {
+				setName(user.name);
+				setEmail(user.email);
+			}
+		}
+	}, [user, userInfo, history, dispatch]);
+
+	const formSubmitHandler = (event) => {
+		event.preventDefault();
+
+		if (password !== confirmPassword) {
+			setMessage("Passwords don't match");
+		} else {
+			// dispatch(register(name, email, password));
+		}
+	};
+
+	return (
+		<Row>
+			<Col md={3}>
+				<h1>Update Profile</h1>
+
+				{/* if error from backend, then show error
+	 else if message from frontend, show message */}
+				{error ? (
+					<Message variant='danger'>{error}</Message>
+				) : message ? (
+					<Message variant='danger'>{message}</Message>
+				) : null}
+				{loading ? (
+					<Loader />
+				) : (
+					<Form onSubmit={formSubmitHandler} className='my-2'>
+						<Form.Group>
+							<Form.Label>Name</Form.Label>
+							<Form.Control
+								type='username'
+								placeholder='Enter Your Name'
+								value={name}
+								onChange={(e) => setName(e.target.value)}
+							></Form.Control>
+						</Form.Group>
+						<Form.Group>
+							<Form.Label>Email Address</Form.Label>
+							<Form.Control
+								type='email'
+								placeholder='Enter Email'
+								value={email}
+								onChange={(e) => setEmail(e.target.value)}
+							></Form.Control>
+						</Form.Group>
+						<Form.Group>
+							<Form.Label>Password</Form.Label>
+							<Form.Control
+								type='password'
+								placeholder='Enter Password'
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+							></Form.Control>
+						</Form.Group>
+						<Form.Group>
+							<Form.Label>Confirm Password</Form.Label>
+							<Form.Control
+								type='password'
+								placeholder='Confirm Password'
+								value={confirmPassword}
+								onChange={(e) => setConfirmPassword(e.target.value)}
+							></Form.Control>
+						</Form.Group>
+						<Button type='submit' variant='dark'>
+							Update
+						</Button>
+					</Form>
+				)}
+			</Col>
+			<Col md={9}></Col>
+		</Row>
+	);
+};
+
+export default ProfileScreen;
