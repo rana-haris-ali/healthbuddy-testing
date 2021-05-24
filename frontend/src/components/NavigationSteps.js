a4dcc68933df74651217f0bb2b5bd96cb95b6084
Rana Haris Ali <64707886+rana-haris-ali@users.noreply.github.com>
2021-05-25 00:51:34 +0500

    create NavigationSteps component

---
+++ frontend/src/components/NavigationSteps.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Nav } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
+// shows links for navigation between specified steps like login shipping payment etc
+const CheckoutSteps = ({ steps, disabledSteps }) => {
+	return (
+		<Nav className='justify-content-center mb-4'>
+			{steps.map((step) => {
+				return (
+					<Nav.Item>
+						<LinkContainer to={step.link}>
+							{/* disable step if included in disabled steps array */}
+							<Nav.Link disabled={disabledSteps.includes(step.name)}>
+								{step.name}
+							</Nav.Link>
+						</LinkContainer>
+					</Nav.Item>
+				);
+			})}
+		</Nav>
+	);
+};
+
+export default CheckoutSteps;
