c4b607b446c4bbd7e59aa91a9a85f669cba9ce9f
Rana Haris Ali <64707886+rana-haris-ali@users.noreply.github.com>
2021-05-24 18:25:32 +0500

    add get and update shipping address case

--- frontend/src/reducers/pharmacy/cartReducers.js
+++ frontend/src/reducers/pharmacy/cartReducers.js
@@ -1,11 +1,20 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../../constants/cartConstants';
+import {
+	CART_ADD_ITEM,
+	CART_GET_SHIPPING_ADDRESS_FAILURE,
+	CART_GET_SHIPPING_ADDRESS_REQUEST,
+	CART_GET_SHIPPING_ADDRESS_SUCCESS,
+	CART_REMOVE_ITEM,
+} from '../../constants/cartConstants';
 
 // cart reduecr checks if item already exists in the cart
 // and if it does then replaces it with new order specs
 
 // if item does not exist then simply adds it to the cart
 
-const cartReducer = (state = { cartItems: [] }, action) => {
+const cartReducer = (
+	state = { cartItems: [], shippingAddress: {} },
+	action
+) => {
 	switch (action.type) {
 		case CART_ADD_ITEM:
 			const item = action.payload;
@@ -35,6 +44,23 @@ const cartReducer = (state = { cartItems: [] }, action) => {
 				),
 			};
 
+		case CART_GET_SHIPPING_ADDRESS_REQUEST:
+			return {
+				...state,
+				loading: true,
+			};
+		case CART_GET_SHIPPING_ADDRESS_SUCCESS:
+			return {
+				...state,
+				loading: false,
+				shippingAddress: action.payload,
+			};
+		case CART_GET_SHIPPING_ADDRESS_FAILURE:
+			return {
+				...state,
+				loading: false,
+				error: action.payload,
+			};
 		default:
 			return state;
 	}
