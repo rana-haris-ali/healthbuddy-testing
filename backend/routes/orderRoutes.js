941e7179ffbd37b2039deff8fe3819b645c37130
Rana Haris Ali <64707886+rana-haris-ali@users.noreply.github.com>
2021-05-28 00:14:10 +0500

    add get single order route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
 import { protect } from '../middleware/authMiddleware.js';
-import { createOrder } from '../controllers/orderController.js';
+import { createOrder, getOrder } from '../controllers/orderController.js';
 
 const router = express.Router();
 
 router.route('/').post(protect, createOrder);
+router.route('/:id').get(protect, getOrder);
 
 export default router;
