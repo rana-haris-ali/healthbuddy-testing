fa319398b91f6b895edf7de249ca779c65bdfdd1
Rana Haris Ali <64707886+rana-haris-ali@users.noreply.github.com>
2021-05-27 07:37:28 +0500

    add currentStep prop to style currentStep link

--- frontend/src/components/NavigationSteps.js
+++ frontend/src/components/NavigationSteps.js
@@ -3,7 +3,7 @@ import { Nav } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 // shows links for navigation between specified steps like login shipping payment etc
-const CheckoutSteps = ({ steps, disabledSteps }) => {
+const CheckoutSteps = ({ steps, disabledSteps, currentStep }) => {
 	return (
 		<Nav className='justify-content-center mb-4'>
 			{steps.map((step) => {
@@ -12,7 +12,11 @@ const CheckoutSteps = ({ steps, disabledSteps }) => {
 						<LinkContainer to={step.link}>
 							{/* disable step if included in disabled steps array */}
 							<Nav.Link disabled={disabledSteps.includes(step.name)}>
-								{step.name}
+								<p
+									style={step.name === currentStep ? { color: 'black' } : null}
+								>
+									{step.name}
+								</p>
 							</Nav.Link>
 						</LinkContainer>
 					</Nav.Item>
