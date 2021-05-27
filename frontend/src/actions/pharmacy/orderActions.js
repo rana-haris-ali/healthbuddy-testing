c6307b7a17353745c6237e2c2f66b24e3c8e42ac
Rana Haris Ali <64707886+rana-haris-ali@users.noreply.github.com>
2021-05-28 00:15:09 +0500

    add getSingleOrder action

--- frontend/src/actions/pharmacy/orderActions.js
+++ frontend/src/actions/pharmacy/orderActions.js
@@ -3,6 +3,9 @@ import {
 	ORDER_CREATE_REQUEST,
 	ORDER_CREATE_SUCCESS,
 	ORDER_CREATE_FAILURE,
+	ORDER_DETAILS_REQUEST,
+	ORDER_DETAILS_SUCCESS,
+	ORDER_DETAILS_FAILURE,
 } from '../../constants/orderConstants';
 
 const createOrder = (order) => async (dispatch, getState) => {
@@ -37,4 +40,35 @@ const createOrder = (order) => async (dispatch, getState) => {
 	}
 };
 
-export { createOrder };
+const getSingleOrder = (id) => async (dispatch, getState) => {
+	try {
+		dispatch({
+			type: ORDER_DETAILS_REQUEST,
+		});
+
+		const { token } = getState().userLogin.userInfo;
+
+		const config = {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		};
+
+		const { data } = await axios.get(`/api/orders/${id}`, config);
+
+		dispatch({
+			type: ORDER_DETAILS_SUCCESS,
+			payload: data,
+		});
+	} catch (error) {
+		dispatch({
+			type: ORDER_DETAILS_FAILURE,
+			payload:
+				error.response && error.response.data.message
+					? error.response.data.message
+					: error.message,
+		});
+	}
+};
+
+export { createOrder, getSingleOrder };
