123ff79a362e7472adc816f34189910d5d0af087
Rana Haris Ali <64707886+rana-haris-ali@users.noreply.github.com>
2021-05-27 09:04:21 +0500

    create order routes

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+import { protect } from '../middleware/authMiddleware.js';
+import { createOrder } from '../controllers/orderController.js';
+
+const router = express.Router();
+
+router.route('/').post(protect, createOrder);
+
+export default router;
