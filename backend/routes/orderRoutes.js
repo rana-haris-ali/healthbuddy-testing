a73b7ca61bccfec3592f461736b76469e7db2556
Rana Haris Ali <64707886+rana-haris-ali@users.noreply.github.com>
2021-06-25 07:54:14 +0500

    add updateToPaid route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,11 +1,16 @@
 import express from 'express';
 
 import { protect } from '../middleware/authMiddleware.js';
-import { createOrder, getOrder } from '../controllers/orderController.js';
+import {
+	createOrder,
+	getOrder,
+	updateOrderToPaid,
+} from '../controllers/orderController.js';
 
 const router = express.Router();
 
 router.route('/').post(protect, createOrder);
 router.route('/:id').get(protect, getOrder);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
 
 export default router;
