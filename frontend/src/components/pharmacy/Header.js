e6dad0fc34e35985ae4c48bc7bd3eb59d30f9184
Rana Haris Ali <64707886+rana-haris-ali@users.noreply.github.com>
2021-07-05 18:25:15 +0500

    update to show admin panel when admin is logged in

--- frontend/src/components/pharmacy/Header.js
+++ frontend/src/components/pharmacy/Header.js
@@ -52,6 +52,19 @@ const Header = () => {
 									</Nav.Link>
 								</LinkContainer>
 							)}
+							{userInfo && userInfo.isAdmin && (
+								<NavDropdown alignRight='true' title='Admin' id='adminMenu'>
+									<LinkContainer to='/admin/userList'>
+										<NavDropdown.Item>Users</NavDropdown.Item>
+									</LinkContainer>
+									<LinkContainer to='/admin/orderList'>
+										<NavDropdown.Item>Orders</NavDropdown.Item>
+									</LinkContainer>
+									<LinkContainer to='/admin/productList'>
+										<NavDropdown.Item>Products</NavDropdown.Item>
+									</LinkContainer>
+								</NavDropdown>
+							)}
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
