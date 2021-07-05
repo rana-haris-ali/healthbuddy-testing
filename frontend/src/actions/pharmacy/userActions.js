2697d55eeee55bb8c859aa0fce2f39cb4b85a40f
Rana Haris Ali <64707886+rana-haris-ali@users.noreply.github.com>
2021-07-05 18:26:07 +0500

    add userList and userDelete actions

--- frontend/src/actions/pharmacy/userActions.js
+++ frontend/src/actions/pharmacy/userActions.js
@@ -15,6 +15,13 @@ import {
 	USER_UPDATE_PROFILE_SUCCESS,
 	USER_UPDATE_PROFILE_FAILURE,
 	USER_DETAILS_RESET,
+	ADMIN_ALL_USERS_LIST_REQUEST,
+	ADMIN_ALL_USERS_LIST_SUCCESS,
+	ADMIN_ALL_USERS_LIST_FAILURE,
+	ADMIN_ALL_USERS_LIST_RESET,
+	ADMIN_DELETE_USER_REQUEST,
+	ADMIN_DELETE_USER_SUCCESS,
+	ADMIN_DELETE_USER_FAILURE,
 } from '../../constants/userConstants';
 
 const login = (email, password) => async (dispatch) => {
@@ -57,6 +64,7 @@ const logout = () => (dispatch) => {
 	dispatch({ type: USER_LOGOUT });
 	dispatch({ type: USER_DETAILS_RESET });
 	dispatch({ type: MY_ORDERS_LIST_RESET });
+	dispatch({ type: ADMIN_ALL_USERS_LIST_RESET });
 };
 
 const register = (name, email, password) => async (dispatch) => {
@@ -197,4 +205,87 @@ const updateUserDetails = (user) => async (dispatch, getState) => {
 	}
 };
 
-export { login, logout, register, getUserDetails, updateUserDetails };
+// get all users list for admin use
+const getAllUserList = () => async (dispatch, getState) => {
+	try {
+		dispatch({
+			type: ADMIN_ALL_USERS_LIST_REQUEST,
+		});
+
+		// get token from state
+		const {
+			userLogin: {
+				userInfo: { token },
+			},
+		} = getState();
+
+		const config = {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		};
+
+		const { data } = await axios.get(`/api/users`, config);
+
+		dispatch({
+			type: ADMIN_ALL_USERS_LIST_SUCCESS,
+			payload: data,
+		});
+	} catch (error) {
+		dispatch({
+			type: ADMIN_ALL_USERS_LIST_FAILURE,
+			payload:
+				error.response && error.response.data.message
+					? error.response.data.message
+					: error.message,
+		});
+	}
+};
+
+// FOR ADMIN USE
+// delete particular user
+const deleteUser = (id) => async (dispatch, getState) => {
+	try {
+		dispatch({
+			type: ADMIN_DELETE_USER_REQUEST,
+		});
+
+		// get token from state
+		const {
+			userLogin: {
+				userInfo: { token },
+			},
+		} = getState();
+
+		const config = {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		};
+
+		const { data } = await axios.delete(`/api/users/${id}`, config);
+
+		dispatch({
+			type: ADMIN_DELETE_USER_SUCCESS,
+			payload: data,
+		});
+	} catch (error) {
+		dispatch({
+			type: ADMIN_DELETE_USER_FAILURE,
+			payload:
+				error.response && error.response.data.message
+					? error.response.data.message
+					: error.message,
+		});
+	}
+};
+
+export {
+	login,
+	logout,
+	register,
+	getUserDetails,
+	updateUserDetails,
+	getAllUserList,
+	deleteUser,
+};
