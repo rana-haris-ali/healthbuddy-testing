f998b8a6b6d44b16716bfde1098c0508a30c5b49
rana-haris-ali <ranaharisali42@yahoo.com>
2021-08-03 23:45:16 +0500

    configure loader to accept props for customization

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -3,21 +3,26 @@ import { Spinner } from 'react-bootstrap';
 
 // Loader component
 
-const Loader = () => {
-  return (
-    <Spinner
-      animation='border'
-      role='status'
-      style={{
-        width: '100px',
-        height: '100px',
-        margin: 'auto',
-        display: 'block',
-      }}
-    >
-      <span className='sr-only'>Loading...</span>
-    </Spinner>
-  );
+const Loader = ({
+	width = '100px',
+	height = '100px',
+	margin = 'auto',
+	display = 'block',
+}) => {
+	return (
+		<Spinner
+			animation='border'
+			role='status'
+			style={{
+				width,
+				height,
+				margin,
+				display,
+			}}
+		>
+			<span className='sr-only'>Loading...</span>
+		</Spinner>
+	);
 };
 
 export default Loader;
