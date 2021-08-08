32f16eca0efcdc4da7a42226a88e75ddac057290
rana-haris-ali <ranaharisali42@yahoo.com>
2021-08-09 01:56:08 +0500

    update reviewSchema

--- backend/models/reviewModel.js
+++ backend/models/reviewModel.js
@@ -4,23 +4,28 @@ import mongoose from 'mongoose';
 //  Will be extended by Patient and Doctor schemas
 
 export const reviewSchema = mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    rating: {
-      type: Number,
-      required: true,
-    },
-    comment: {
-      type: String,
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-  }
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		rating: {
+			type: Number,
+			required: true,
+		},
+		comment: {
+			type: String,
+			required: true,
+		},
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			required: true,
+			ref: 'User',
+		},
+	},
+	{
+		timestamps: true,
+	}
 );
 
 export default mongoose.model('Review', reviewSchema);
