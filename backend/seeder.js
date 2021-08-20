50f9cc3b35d09965c0a37459c4ab721b63791133
rana-haris-ali <ranaharisali42@yahoo.com>
2021-08-20 08:01:21 +0500

    add Patient and Doctor to delete script

--- backend/seeder.js
+++ backend/seeder.js
@@ -6,6 +6,8 @@ import products from './data/products.js';
 import User from './models/userModel.js';
 import Product from './models/productModel.js';
 import Order from './models/orderModel.js';
+import Patient from './models/patientModel.js';
+import Doctor from './models/doctorModel.js';
 import connectDB from './config/db.js';
 
 dotenv.config({ path: '../.env' });
@@ -13,45 +15,47 @@ dotenv.config({ path: '../.env' });
 connectDB();
 
 const importData = async () => {
-  try {
-    await User.deleteMany();
-    await Product.deleteMany();
-    await Order.deleteMany();
+	try {
+		await User.deleteMany();
+		await Product.deleteMany();
+		await Order.deleteMany();
 
-    const insertedUsers = await User.insertMany(users);
+		const insertedUsers = await User.insertMany(users);
 
-    // admin user is the first user entry
-    const adminUserID = insertedUsers[0]._id;
+		// admin user is the first user entry
+		const adminUserID = insertedUsers[0]._id;
 
-    const productsWithAdmin = products.map((product) => {
-      return { ...product, user: adminUserID };
-    });
+		const productsWithAdmin = products.map((product) => {
+			return { ...product, user: adminUserID };
+		});
 
-    await Product.insertMany(productsWithAdmin);
+		await Product.insertMany(productsWithAdmin);
 
-    console.log('Data Imported'.green);
-    process.exit();
-  } catch (error) {
-    console.log(`Error: ${error}`.red);
-    process.exit(1);
-  }
+		console.log('Data Imported'.green);
+		process.exit();
+	} catch (error) {
+		console.log(`Error: ${error}`.red);
+		process.exit(1);
+	}
 };
 
 const deleteData = async () => {
-  try {
-    await User.deleteMany();
-    await Product.deleteMany();
-    await Order.deleteMany();
+	try {
+		await User.deleteMany();
+		await Product.deleteMany();
+		await Order.deleteMany();
+		await Patient.deleteMany();
+		await Doctor.deleteMany();
 
-    console.log('Data deleted'.yellow);
-    process.exit();
-  } catch (error) {
-    console.log(`Error: ${error}`.red);
-    process.exit(1);
-  }
+		console.log('Data deleted'.yellow);
+		process.exit();
+	} catch (error) {
+		console.log(`Error: ${error}`.red);
+		process.exit(1);
+	}
 };
 if (process.argv[2] === '-d') {
-  deleteData();
+	deleteData();
 } else {
-  importData();
+	importData();
 }
