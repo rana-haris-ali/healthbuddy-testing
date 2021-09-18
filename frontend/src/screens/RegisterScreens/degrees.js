4e959ca9f4c0f92fd65ef2191027ca79877559a9
rana-haris-ali <ranaharisali42@yahoo.com>
2021-09-19 00:32:04 +0500

    make diseases, degrees files for rendering options

---
+++ frontend/src/screens/RegisterScreens/degrees.js
@@ -0,0 +1,8 @@
+const degrees = [
+	{ value: 'MBBS', label: 'MBBS' },
+	{ value: 'FCPS', label: 'FCPS' },
+	{ value: 'FRPS', label: 'FRPS' },
+	{ value: 'MCPS', label: 'MCPS' },
+];
+
+export default degrees;
---
+++ frontend/src/screens/RegisterScreens/diseases.js
@@ -0,0 +1,10 @@
+const diseases = [
+	{ value: 'Cancer', label: 'Cancer' },
+	{ value: 'Covid-19', label: 'Covid-19' },
+	{ value: 'Diabetes', label: 'Diabetes' },
+	{ value: 'Arthiritis', label: 'Arthiritis' },
+	{ value: 'Blindness', label: 'Blindness' },
+	{ value: 'Deafness', label: 'Deafness' },
+];
+
+export default diseases;
